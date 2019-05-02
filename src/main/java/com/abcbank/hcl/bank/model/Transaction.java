bfe3d41ea0af89dd98f0cb2fa01ad4da558af785
= <siva3060@gmail.com>
2019-05-02 16:18:22 +0530

    implementd methods to search by criteria

--- src/main/java/com/abcbank/hcl/bank/model/Transaction.java
+++ src/main/java/com/abcbank/hcl/bank/model/Transaction.java
@@ -2,7 +2,6 @@ package com.abcbank.hcl.bank.model;
 
 import java.time.LocalDate;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
--- src/main/java/com/abcbank/hcl/bank/repository/TranscationRepository.java
+++ src/main/java/com/abcbank/hcl/bank/repository/TranscationRepository.java
@@ -3,7 +3,6 @@ package com.abcbank.hcl.bank.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 import com.abcbank.hcl.bank.model.Transaction;
 
 @Repository
